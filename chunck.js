98afad5e7cbdca2cd7245845f513b2a987abdcce
mmonis440 <mmonis440@gmail.com>
2023-02-09 01:24:23 +0530

    chunk array slice

---
+++ chunck.js
@@ -0,0 +1,11 @@
+function sliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        const chunk = arr.slice(i, i + chunkSize);
+        res.push(chunk);
+    }
+    return res;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(sliceIntoChunks(arr, 5));
