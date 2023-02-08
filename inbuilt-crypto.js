a5fcd09e5c87df219f1f58de0bf5e4fb339da88e
mmonis440 <mmonis440@gmail.com>
2023-02-09 01:15:53 +0530

    type:Module in package.json

--- inbuilt-crypto.js
+++ inbuilt-crypto.js
@@ -1,14 +1,34 @@
-const crypto = require('crypto');
-const algorithm = 'aes 192 cbc';
-const password = "passowrd used to generate key";
+import { Buffer } from 'node:buffer';
+const {
+  scryptSync,
+  createDecipheriv,
+} = await import('node:crypto');
 
-const key = crypto.scryptSync(password,'salt',24);
-const iv=Buffer.alloc(16,0)
-const decipher = crypto.createDecipheriv(algorithm,key,iv);
+const algorithm = 'aes-192-cbc';
+const password = 'Password used to generate key';
+// Key length is dependent on the algorithm. In this case for aes192, it is
+// 24 bytes (192 bits).
+// Use the async `crypto.scrypt()` instead.
+const key = scryptSync(password, 'salt', 24);
+// The IV is usually passed along with the ciphertext.
+const iv = Buffer.alloc(16, 0); // Initialization vector.
 
-const encrypted="'e5f79c5915c02171eec6b212d5520d44480993d7d622a7c4c2da32f6efda0ffa";
+const decipher = createDecipheriv(algorithm, key, iv);
 
-let fail = decipher.update(encrypted,'hex','utf8');
+let decrypted = '';
+decipher.on('readable', () => {
+  let chunk;
+  while (null !== (chunk = decipher.read())) {
+    decrypted += chunk.toString('utf8');
+  }
+});
+decipher.on('end', () => {
+  console.log(decrypted);
+  // Prints: some clear text data
+});
 
-fail +=decipher.final('utf8');
-console.log(final);
+// Encrypted with same algorithm, key and iv.
+const encrypted =
+  'e5f79c5915c02171eec6b212d5520d44480993d7d622a7c4c2da32f6efda0ffa';
+decipher.write(encrypted, 'hex');
+decipher.end();
